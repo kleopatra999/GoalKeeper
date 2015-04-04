1bee10a22ee40787132261e9d1b276665476ccee
mrbimc <pavel.sikun@gmail.com>
2015-04-04 21:23:39 +0300

    --

--- app/src/main/java/com/yaropav/goalkeeper/fragments/ChainFragment.java
+++ app/src/main/java/com/yaropav/goalkeeper/fragments/ChainFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -36,17 +37,57 @@ public class ChainFragment extends Fragment {
 
         mChain = (Chain) getArguments().getSerializable(CHAIN_KEY);
 
-        ((TextView) v.findViewById(R.id.checkbox_title)).setText(mChain.getName());
-        ((CheckBox) v.findViewById(R.id.checkbox)).setChecked(true);
-
-        ((TextView) v.findViewById(R.id.seekbar_title)).setText("Weekly goals");
-        SeekBar seekBar = (SeekBar) v.findViewById(R.id.seekbar);
-        seekBar.setMax(7);
+        setAskForNote(v);
 
         EditText name = (EditText) v.findViewById(R.id.name_edittext);
         name.setText(mChain.getName());
 
         return v;
+    }
+
+
+    private void setAskForNote(View v) {
+        final CheckBox checkBox = (CheckBox) v.findViewById(R.id.checkbox);
+        View holder = v.findViewById(R.id.checkbox_holder);
+
+        holder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkBox.toggle();
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //todo save to prefs
+            }
+        });
+    }
+
+    private void setSeekbarPreference(View v) {
+        SeekBar seekBar = (SeekBar) v.findViewById(R.id.seekbar);
+        seekBar.setMax(7);
+
+        final TextView seekBarValue = (TextView) v.findViewById(R.id.seekbar_value);
+        seekBarValue.setText(mChain.getWeekAim() + " days");
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarValue.setText(progress + " days");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
 
