e11c6690a38400f48a01496c14c2bd66fa83da1d
yarolegovich <yarolegovich@gmail.com>
2015-04-04 13:23:07 +0300

    Added broadcast receiver

---
+++ app/src/main/java/com/yaropav/goalkeeper/GoalCheckReceiver.java
@@ -0,0 +1,15 @@
+package com.yaropav.goalkeeper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Ярик on 04.04.2015.
+ */
+public class GoalCheckReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/com/yaropav/goalkeeper/MainActivity.java
+++ app/src/main/java/com/yaropav/goalkeeper/MainActivity.java
@@ -1,5 +1,9 @@
 package com.yaropav.goalkeeper;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,28 +16,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        Intent intent = new Intent(this, GoalCheckReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 0, intent, 0);
+        am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime(),
+                AlarmManager.INTERVAL_DAY,
+                broadcast);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
